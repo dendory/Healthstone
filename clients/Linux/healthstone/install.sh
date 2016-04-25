76658e8d1eb94ba4decb6760d7e973626336c7d7
Patrick Lambert <dendory@gmail.com>
2016-04-25 13:17:12 -0400

    install woes

--- clients/Linux/healthstone/install.sh
+++ clients/Linux/healthstone/install.sh
@@ -1,6 +1,8 @@
 cp healthstone.py /usr/bin/healthstone.py
 chmod +x /usr/bin/healthstone.py
-crontab -l > mycron
-echo "*/5 * * * * python3 /usr/bin/healthstone.py" >> mycron
-crontab mycron
-rm -f mycron
+crontab -l > /tmp/mycron
+if ! grep -q healthstone /tmp/mycron; then
+        echo "*/5 * * * * /usr/bin/healthstone.py" >> /tmp/mycron
+        crontab /tmp/mycron
+fi
+rm -f /tmp/mycron
