38d284d007a5f07561ab5578909d0dc386acb67c
Patrick Lambert <dendory@gmail.com>
2016-04-25 09:37:06 -0400

    Updated signature

--- src/make.bat
+++ src/make.bat
@@ -1,3 +1,3 @@
 :: Make a CSharp service and install it
-@echo off
-%SYSTEMROOT%\Microsoft.NET\Framework64\v4.0.30319\csc.exe /out:%~dp0..\clients\Windows\healthstone\healthstone.exe %~dp0healthstone.cs
+@%SYSTEMROOT%\Microsoft.NET\Framework64\v4.0.30319\csc.exe /out:%~dp0..\clients\Windows\healthstone\healthstone.exe %~dp0healthstone.cs
+@call "C:\Program Files (x86)\Windows Kits\8.0\bin\x64\signtool.exe" sign /n "Patrick Lambert" /t http://timestamp.verisign.com/scripts/timstamp.dll %~dp0..\clients\Windows\healthstone\healthstone.exe
